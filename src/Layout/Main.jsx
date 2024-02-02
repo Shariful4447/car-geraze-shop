0500bfb6123e32dbb833b6066df79973691b7098
Shariful4447 <shariful.islam4447@gmail.com>
2024-02-02 21:46:59 +0100

    Navbar and Footer added

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Pages/SharedPages/Footer/Footer";
+import NavBar from "../Pages/SharedPages/NavBar/NavBar";
+
+
+
+
+const Main = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+           <Outlet></Outlet>
+           <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
