77516d309d7b4ee4a8eaebf47d774106bf437641
Shariful4447 <shariful.islam4447@gmail.com>
2024-02-03 10:58:56 +0100

    Navbar design done

--- src/Pages/SharedPages/NavBar/NavBar.jsx
+++ src/Pages/SharedPages/NavBar/NavBar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 const NavBar = () => {
@@ -10,16 +11,32 @@ const NavBar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Homepage</a></li>
-                        <li><a>Portfolio</a></li>
+                        <li><a>Home</a></li>
                         <li><a>About</a></li>
+                        <li><a>Services</a></li>
+                        <li><a>Blog</a></li>
+                        <li><a>Contact</a></li>
                     </ul>
                     </div>
                 </div>
-                <div className="navbar-center">
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
-                </div>
+                <Link className="navbar-center">
+                    <a className="btn btn-ghost text-xl">Home</a>
+                </Link>
+                <Link className="navbar-center">
+                    <a className="btn btn-ghost text-xl">About</a>
+                </Link>
+                <Link className="navbar-center">
+                    <a className="btn btn-ghost text-xl">Services</a>
+                </Link>
+                <Link className="navbar-center">
+                    <a className="btn btn-ghost text-xl">Blog</a>
+                </Link>
+                <Link className="navbar-center">
+                    <a className="btn btn-ghost text-xl">Contact</a>
+                </Link>
+                
                 <div className="navbar-end">
+                    
                     <button className="btn btn-ghost btn-circle">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                     </button>
@@ -29,6 +46,9 @@ const NavBar = () => {
                         <span className="badge badge-xs badge-primary indicator-item"></span>
                     </div>
                     </button>
+                    <Link className="border-2 border-black text-red-500 text-xl">
+                        <a className="btn btn-ghost text-xl">Appointment</a>
+                    </Link>
                 </div>
             </div>
             
