3a53938b6be0b720bc17c7583c9cb55b329c947d
Shariful4447 <shariful.islam4447@gmail.com>
2024-02-04 13:02:44 +0100

    image style in about section done

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -4,14 +4,19 @@ import aboutParts from '../../assets/images/about_us/parts.jpg'
 
 const AboutUs = () => {
     return (
-        <div className="grid grid-cols-2">
-            <div>
-                <h2>Image section</h2>
-                <img src={about} alt="" />
-                <img className='w-72 place-content-end -mt-48 boder-4 border-solid border-orange-600 p-4' src={aboutParts} alt="" />
-            </div>
-            <div>
-                <h2>This is description section</h2>
+        <div>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div className='lg:w-1/2 relative'>
+                        <img src={about} className="w-3/4 rounded-lg shadow-2xl" />
+                        <img src={aboutParts} className="w-1/2 absolute right-5 top-1/2 border-8 border-white rounded-lg shadow-2xl" />
+                    </div>
+                    <div className='lg:w-1/2'>
+                        <h1 className="text-5xl font-bold">Box Office News!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
